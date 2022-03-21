8c75402033f8b7bda76537ea7bb509339eba00e1
Jessica Chang <jesschang77@gmail.com>
2022-03-21 16:04:21 -0700

    Add frontend

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ morgan.token('requestData', function(req) {
 })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :requestData'))
 
